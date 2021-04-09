2b7fd7b68b1f27af3785bb2959b99440941ba4f5
S.m.Samrat <imsamrat24@gmail.com>
2021-04-09 23:14:44 +0600

    Background Image added in Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -19,13 +19,13 @@ const Navbar = () => {
                             <a class="nav-link ms-5 mr-5" href="#">Dental Services</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link ms-5 mr-5" href="#">Reviews</a>
+                            <a class="nav-link ms-5 mr-5 text-white" href="#">Reviews</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link ms-5 mr-5" href="#">Blogs</a>
+                            <a class="nav-link ms-5 mr-5 text-white" href="#">Blogs</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link ms-5 mr-5" href="#">Contact Us</a>
+                            <a class="nav-link ms-5 mr-5 text-white" href="#">Contact Us</a>
                         </li>
                     </ul>
                 </div>
